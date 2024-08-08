2f93307088a2b7ad6fc4c14aaa9d8a8d0658be67
lamees-fayez <lameesfayez@gmail.com>
2024-08-08 21:04:52 +0300

    Added exam.py

---
+++ exam.py
@@ -0,0 +1,20 @@
+a = ('1) Add new employee')
+b = ('2) Print all employees')
+c = ('3) Delete by age')
+d = ('4) Update salary by name')
+e = ('5) End program')
+print( a ,b ,c ,d ,e)
+chioce= input()
+while True:
+    if chioce == '1':
+        print(a)
+    elif chioce == '2':
+        print(b)
+    elif chioce == '3':
+        print(c)
+    elif chioce =='4':
+        print(d)
+    elif chioce == '5':
+        break 
+    else:
+        print('Invalid iput try again')
