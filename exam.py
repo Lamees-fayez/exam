31921ed1a8b7ec5ac543ccda611a30b23c3a9a58
lamees-fayez <lameesfayez@gmail.com>
2024-08-10 21:07:57 +0300

    Added exam.py

--- exam.py
+++ exam.py
@@ -1,27 +1,23 @@
-
-a = ('1) Add new employee')
-b = ('2) Print all employees')
-c = ('3) Delete by age')
-d = ('4) Update salary by name')
-e = ('5) End program')
-print( a ,b ,c ,d ,e)
+main_program= {'1) Add new employee',
+'2) Print all employees',
+'3) Delete by age',
+'4) Update salary by name',
+'5) End program'
+}
+print( main_program)
 chioce= input()
-if chioce == '1':
-    print(a)
-elif chioce == '2':
-    print(b)
-elif chioce == '3':
-    print(c)
-elif chioce =='4':
-    print(d)
-elif chioce == '5':
-    print('End program')
-break
-else:
-    print('Invalid iput try again.')
-    #choices        
-a= 1
-b= 2
-c= 3
-d= 4
-e= 5
+for i in main_program:
+    if chioce == '1':
+        print(a)
+    elif chioce == '2':
+         print(b)
+    elif chioce == '3':
+         print(c)
+    elif chioce =='4':
+         print(d)
+    elif chioce == '5':
+        print('End program')
+    break
+    else :
+        print('Invalid iput try again.')
+    
