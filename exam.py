7c085b4131e5a02f0d8a2195504128fc84e65b08
lamees-fayez <lameesfayez@gmail.com>
2024-08-12 19:58:44 +0300

    Added exam.py

--- exam.py
+++ exam.py
@@ -1,36 +1,36 @@
-main_program= { 
+main_program= [
 '1) Add new employee',
 '2) Print all employees',
 '3) Delete by age',
 '4) Update salary by name',
 '5) End program'
-}
-print( main_program)
-a= ('1) Add new employee')
-b =('2) Print all employees')
-c=('3) Delete by age')
-d=('4) Update salary by name')
-e=('5) End program')
+]
 
+print( main_program)
 chioce= input()
+
+
 for i in main_program:
     if chioce == '1':
-        print(a) ,
+        print(main_program[0]) ,
         employee_data= {'name'==(input('Enter the name:'),
                         'age'== input('Enter the age:'),
                         'salary'== input('Enter the salary:'))}
         print(main_program)
-        continue    
+        chioce= input()
     elif chioce == '2':
-         print(b)
-         print(name , age , salary)
+         print(main_program[1])
+         print(employee_data)
+         chioce= input()
     elif chioce == '3':
-         print(c)
+         print(main_program[2])
+         chioce= input()
     elif chioce =='4':
-         print(d)
+         print(main_program[3])
+         chioce= input()
     elif chioce == '5':
-        print('End program')
+        print(main_program[4])
         break
     else :
-        print('Invalid iput try again.')
+        print('Invalid input try again.')
     
