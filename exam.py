0baaf3d84b9b7b4c2876ce88895c09cb2d013abd
lamees-fayez <lameesfayez@gmail.com>
2024-08-14 20:58:30 +0300

    Added exam.py

--- exam.py
+++ exam.py
@@ -5,31 +5,48 @@ main_program= [
 '4) Update salary by name',
 '5) End program'
 ]
-
-print( main_program)
-chioce= input()
-
-
-for i in main_program:
+employees=[]
+while True:
+    print(main_program)
+    chioce= input()
     if chioce == '1':
         print(main_program[0]) ,
-        employee_data= {'name'==(input('Enter the name:'),
-                        'age'== input('Enter the age:'),
-                        'salary'== input('Enter the salary:'))}
-        print(main_program)
-        chioce= input()
+        name = (input('Enter employee name :    ').strip().capitalize() )
+        age= int (input('Enter employee age :     ').strip())
+        salary= int(input('Enter employee salary :     ').strip())
+        employees.append({'name': name ,
+         'age':age ,
+          'salary': salary})
+        print('employee Added')
+        print(employees)
     elif chioce == '2':
-         print(main_program[1])
-         print(employee_data)
-         chioce= input()
+        print(main_program[1])
+        print('Employees list:')
+        for emp in employees:
+            print(f"Employee name {emp['name']} his age is {emp['age']} his salary is {emp['salary']}")
+
     elif chioce == '3':
-         print(main_program[2])
-         chioce= input()
+        print(main_program[2])
+        age_from= int(input('Enter age from : ').strip())
+        age_to= int(input('Enter age to : ').strip())
+        for i in range (age_from , age_to+1):
+            for emp in employees :
+                print(f"Deleting {emp['name']}")
+                employees.remove(emp)
+
     elif chioce =='4':
-         print(main_program[3])
-         chioce= input()
+        print(main_program[3])
+        name= (input('Enter employee name:').capitalize().strip())
+        new_salary=int(input('Enter new salary'))
+        for emp in employees:
+            if emp ['name']== name:
+                emp['salary']== new_salary
+                print(f'Employee {emp["name"]} updated' ) 
+            else:
+                print('Employee not found')
     elif chioce == '5':
-        print(main_program[4])
+        
+        print('**End program**')
         break
     else :
         print('Invalid input try again.')
